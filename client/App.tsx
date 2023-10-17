3794707c938d8e83d17036999ef37e0386f1f4d9
quinnCraddock4 <quinncraddock@DESKTOP-UI3TT7Q.localdomain>
2023-10-16 19:00:28 -0500

    tailwind is goated

--- client/App.tsx
+++ client/App.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import './input.css';
 
 const App: React.FC = () => {
-  return <p>OK WE DID IT</p>;
+  return <p className='text-3xl text-red-500'>OK WE DID IT</p>;
 };
 
 export default App;
