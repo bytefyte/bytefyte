61d52a1213e0f5ab80790bf1e7f3e86c26f1e7be
Max Weiner <maxweiner02@gmail.com>
2023-10-17 15:44:07 -0600

    setup websocket.io, and created Home page
    Co-authored-by: Choebryan <Choebryan@users.noreply.github.com>
    Co-authored-by: Nathan Doucette <fluffyandnathan@gmail.com>
    Co-authored-by: Siddhant Saxena <sidsaxena27@users.noreply.github.com>

--- client/App.tsx
+++ client/App.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './input.css';
 import Login from './routes/Login';
 import Signup from './routes/Signup';
 import { Routes, Route, useNavigate, BrowserRouter } from 'react-router-dom';
 import Home from './routes/Home';
 const App: React.FC = () => {
+  const [username, setUsername] = useState('');
   return (
     <Routes>
-      <Route path='/' element={<Login />} />
-      <Route path='/signup' element={<Signup />} />
-      <Route path='/home' element={<Home />}></Route>
+      <Route path='/' element={<Login username={username} setUsername={setUsername}/>} />
+      <Route path='/signup' element={<Signup username={username} setUsername={setUsername}/>} />
+      <Route path='/home' element={<Home username={username} setUsername={setUsername}/>}></Route>
     </Routes>
   );
 };
