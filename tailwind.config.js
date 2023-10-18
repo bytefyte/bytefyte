b58f5cea66c72a9b5dfdf7b8b5185d854eda58f5
quinnCraddock4 <quinncraddock@DESKTOP-UI3TT7Q.localdomain>
2023-10-17 19:00:40 -0500

    chat feature

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ['./client/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
+      backgroundImage: theme => ({
+        'custom-pattern': `repeating-linear-gradient(45deg, ${theme(
+          'colors.primary',
+        )} 10px, ${theme('colors.default')} 20px)`,
+      }),
       backgroundColor: {
         animated:
           'linear-gradient(60deg, rgba(84, 58, 183, 1) 0%, rgba(0, 172, 193, 1) 100%)',
