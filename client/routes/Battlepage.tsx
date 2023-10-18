9539b9084f2c4926e31d698bf4308e1fc8e4e642
quinnCraddock4 <quinncraddock@DESKTOP-UI3TT7Q.localdomain>
2023-10-17 20:13:32 -0500

    code now runs and gets executed runs fizzBuzz test as of now

--- client/routes/Battlepage.tsx
+++ client/routes/Battlepage.tsx
@@ -61,7 +61,7 @@ const BattlePage = () => {
           </button>
         </div>
       </div>
-      <div className='absolute top-0 right-0 w-1/2 h-screen bg-gray-200'>
+      <div className='absolute top-0 right-0 w-1/2 h-7/8 h-screen bg-gray-200'>
         <div className='p-2'>
           <PreviewPopup></PreviewPopup>
         </div>
